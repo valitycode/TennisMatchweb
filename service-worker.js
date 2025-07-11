15bd5facb82832b8c5f9292cc9b9335b50512a14
valitycode <marcantt2008@gmail.com>
2025-07-11 13:47:08 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,23 @@
-const CACHE_NAME = 'tennis-match-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/app.js',
-  '/manifest.json',
-  '/sound/bip.mp3',
-  '/icon-192.png',
-  '/icon-512.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('tennis-match-cache').then(cache => {
+      return cache.addAll([
+        '/TennisMatchweb/',
+        '/TennisMatchweb/index.html',
+        '/TennisMatchweb/style.css',
+        '/TennisMatchweb/app.js',
+        '/TennisMatchweb/manifest.json',
+        '/TennisMatchweb/icon-192.png',
+        '/TennisMatchweb/icon-512.png'
+      ]);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
   );
 });
